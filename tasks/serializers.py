d57b75c0cad239b934c9c12f6ff5a82735a76e57
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-16 18:20:12 -0600

    updated serializer to work with rich text editor

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import Task
+from bleach import clean
+
+allowed_tags = ["p", "br", "strong", "h1", "h2", "em", "b", "i", "u", "ul", "ol", "li"]
 
 class TaskSerializer(serializers.ModelSerializer):
+    def validate_description(self, value):
+        return clean(value, tags=allowed_tags)
     class Meta:
         model = Task
         fields = '__all__'
