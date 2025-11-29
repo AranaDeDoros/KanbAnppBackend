f7a8587cfcf3c892d96c6d008d0c600a715511e4
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-29 15:43:38 -0600

    project model update

--- projects/serializers.py
+++ projects/serializers.py
@@ -2,8 +2,14 @@ from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Project model.
+    The 'owner' field is set as read-only to prevent changes via API requests,
+    ensuring that ownership can only be set internally.
+    """
     owner = serializers.StringRelatedField()
 
     class Meta:
         model = Project
         fields = '__all__'
+        read_only_fields = ["owner"]
--- tasks/models.py
+++ tasks/models.py
@@ -1,19 +1,10 @@
 from django.db import models
 from django.conf import settings
 from projects.models import Project
+from common.choices import STATUS_CHOICES
+from common.choices import PRIORITIES
 
 class Task(models.Model):
-    STATUS_CHOICES = [
-        ('backlog', 'Backlog'),
-        ('wip', 'In Progress'),
-        ('done', 'Completed'),
-    ]
-
-    PRIORITIES = [
-        ('high', 'High'),
-        ('regular', 'Regular'),
-        ('low', 'Low'),
-    ]
 
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
