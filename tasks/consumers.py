6df49fe70efa03da2417a67ecab4e4ddbcc36fd1
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-30 01:25:47 -0600

    clean up

--- tasks/consumers.py
+++ tasks/consumers.py
@@ -3,7 +3,6 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class TaskConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Unir al grupo común de tareas (podrías tenerlo por proyecto)
         self.room_group_name = "tasks_updates"
 
         await self.channel_layer.group_add(
@@ -21,9 +20,7 @@ class TaskConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         data = json.loads(text_data)
-        # Aquí podrías manejar mensajes entrantes (no requerido si solo es broadcast)
         print("Received:", data)
 
     async def task_update(self, event):
-        """Recibe actualizaciones desde el backend y las manda al frontend"""
         await self.send(text_data=json.dumps(event["data"]))
