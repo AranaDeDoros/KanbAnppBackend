1d7e12de77df735a4864475206f8f04dbf87d6e0
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-12-01 23:28:11 -0600

    updated serializer to handle assigned to and return a user for the frontend

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,24 +1,31 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Task
 from bleach import clean
 
+User = get_user_model()
+
 allowed_tags = ["p", "br", "strong", "h1", "h2", "em", "b", "i", "u", "ul", "ol", "li"]
 
-
-from django.contrib.auth import get_user_model
-from rest_framework import serializers
-
-User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "username", "email"]
 
+
 class TaskSerializer(serializers.ModelSerializer):
-    assigned_to = UserSerializer(read_only=True)
-    def validate_description(self, value):
-        return clean(value, tags=allowed_tags)
+    assigned_to = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        required=False,
+        allow_null=True
+    )
+
+    assigned_to_user = UserSerializer(source="assigned_to", read_only=True)
+
     class Meta:
         model = Task
-        fields = '__all__'
+        fields = "__all__"
+
+    def validate_description(self, value):
+        return clean(value, tags=allowed_tags)
