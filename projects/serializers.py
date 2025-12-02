f9ca94e0721169dfcb9f3367c8e87092b5c36810
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-12-01 21:22:45 -0600

    task-project-user serialized relationship fixed

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Project
+from tasks.serializers import TaskSerializer
 
 class ProjectSerializer(serializers.ModelSerializer):
     """
@@ -7,6 +8,7 @@ class ProjectSerializer(serializers.ModelSerializer):
     The 'owner' field is set as read-only to prevent changes via API requests,
     ensuring that ownership can only be set internally.
     """
+    tasks = TaskSerializer(many=True, read_only=True)
     owner = serializers.StringRelatedField()
 
     class Meta:
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,7 +4,19 @@ from bleach import clean
 
 allowed_tags = ["p", "br", "strong", "h1", "h2", "em", "b", "i", "u", "ul", "ol", "li"]
 
+
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
+
 class TaskSerializer(serializers.ModelSerializer):
+    assigned_to = UserSerializer(read_only=True)
     def validate_description(self, value):
         return clean(value, tags=allowed_tags)
     class Meta:
