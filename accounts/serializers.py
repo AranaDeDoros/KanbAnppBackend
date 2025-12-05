9824354bbdd0b4261cd5fa1fba81bab73835392f
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-12-04 22:19:10 -0600

    skeleton for roles, will work with admin and regular users for now

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import Account
+from django.contrib.auth.models import User
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ['id', 'username', 'display_name', 'bio', 'email']
+
+class UserSerializer(serializers.ModelSerializer):
+    isAdmin = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "isAdmin"]
+
+    def get_isAdmin(self, obj):
+        return obj.roles.filter(name="Admin").exists()
--- projects/views.py
+++ projects/views.py
@@ -2,13 +2,11 @@ from rest_framework import viewsets, permissions
 from .models import Project
 from .serializers import ProjectSerializer
 from django.db.models import Q
-
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # Solo proyectos donde el usuario es owner o miembro
         user = self.request.user
         return Project.objects.filter(Q(owner=user) | Q(members=user)).distinct()
 
