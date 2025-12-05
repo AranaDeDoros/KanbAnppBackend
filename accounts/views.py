c60b6ac1b93adbc70453a938b62f627bb3c9fb89
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-12-04 23:24:19 -0600

    updated serializers references

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -13,16 +12,10 @@ def current_user(request):
     Return the currently authenticated user's info.
     """
     user = request.user
-    return Response({
-        "id": user.id,
-        "username": user.username,
-        "email": user.email,
-    })
+    return Response(UserSerializer(user).data)
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def users(request):
-    user_list = User.objects.all()
-    users_data = [{"id": user.id, "username": user.username, "email": user.email}
-                  for user in user_list]
-    return Response(users_data)
+    queryset = User.objects.all()
+    return Response(UserSerializer(queryset, many=True).data)
