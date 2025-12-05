52e07c97fa4cfa671b7e0f1d1305a656451f654c
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-12-04 23:22:43 -0600

    restricting cud ops to regular users

--- projects/management/commands/clean_users.py
+++ projects/management/commands/clean_users.py
@@ -7,11 +7,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         User.objects.all().delete()
-        Role.objects.create(name='admin')
+        Role.objects.get_or_create(name=Role.ADMIN)
+        Role.objects.get_or_create(name=Role.COLLABORATOR)
         admin =User.objects.create_user(username='admin', password='testpass', is_superuser=True, is_staff=True)
-        admin.roles.add(Role.objects.get(name='admin'))
+        admin.roles.add(Role.objects.get(name=Role.ADMIN))
         usr1= User.objects.create_user(username='testuser1', password='testpass')
+        usr1.roles.add(Role.objects.get(name=Role.COLLABORATOR))
         usr2 = User.objects.create_user(username='testuser2', password='testpass')
+        usr2.roles.add(Role.objects.get(name=Role.COLLABORATOR))
         for user in User.objects.all():
             print(f"User: {user.username}, ID: {user.id}, Roles: {[role.name for role in user.roles.all()]} ")
         self.stdout.write(self.style.SUCCESS(f"Deleted records"))
--- projects/views.py
+++ projects/views.py
@@ -2,9 +2,12 @@ from rest_framework import viewsets, permissions
 from .models import Project
 from .serializers import ProjectSerializer
 from django.db.models import Q
+from rest_framework.permissions import IsAuthenticated
+from accounts.models import IsAdminRole
+
 class ProjectViewSet(viewsets.ModelViewSet):
     serializer_class = ProjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    #permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
@@ -12,3 +15,10 @@ class ProjectViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    def get_permissions(self):
+        admin_actions = ["create", "update", "partial_update", "destroy"]
+        if self.action in admin_actions:
+            return [IsAuthenticated(), IsAdminRole()]
+        #view okay for regulars
+        return [IsAuthenticated()]
