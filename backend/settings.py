71db611d454b0e930563deed27010d4029896201
AranaDeDoros <37278601+AranaDeDoros@users.noreply.github.com>
2026-08-22 01:05:46 -0600

    sequence

--- backend/settings.py
+++ backend/settings.py
@@ -89,7 +89,7 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-CORS_ALLOWED_ORIGINS = [config('DJANGO_ALLOWED_HOSTS', default='http://localhost:3000').split(',')]
+CORS_ALLOWED_ORIGINS = [config('DJANGO_ALLOWED_HOSTS', default=['http://localhost:3000'])]
 
 
 LANGUAGE_CODE = 'en-us'
